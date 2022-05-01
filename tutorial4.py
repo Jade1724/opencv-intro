95cb6a4613893a05bfbd9b0ef6adba1b48b9c58a
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 20:45:45 +1200

    Drew a rectangle on a captured image

--- tutorial4.py
+++ tutorial4.py
@@ -15,6 +15,10 @@ while True:
     img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
     img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
 
+    # Drawing a rectangle
+    # Pass the source image, center points, radius, color, and line thicknes or -1 to fill
+    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
+
     cv2.imshow('Frame', img)
 
     if cv2.waitKey(1) == ord('q'):
