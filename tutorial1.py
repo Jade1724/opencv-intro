e65b6086d722e4457ab601308f8d409d679f9e1f
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 19:43:05 +1200

    Writing output image as a new image.

--- tutorial1.py
+++ tutorial1.py
@@ -4,6 +4,7 @@ img = cv2.imread('assets/logo.png', 1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
 
+cv2.imwrite('new_img.jpg', img)
 cv2.imshow('Image', img)
 
 # Wait for any key press for infinite time
