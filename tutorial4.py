7c05fafca3c74ce742e132906b4474b58c95efaf
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 20:42:58 +1200

    Drew lines on the captured image

---
+++ tutorial4.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # Drawing a line
+    # Coordinate system in OpenCV starts from left top as (x, y) = (0, 0)
+    # Pass the source image, starting position, ending position, color, thickness
+    img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
+    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+
+    cv2.imshow('Frame', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
