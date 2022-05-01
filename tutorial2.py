3482920a59923054c5f9a0d7e43d0ec04ff4e0e4
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 19:52:00 +1200

    Pixels in OpenCV

--- tutorial2.py
+++ tutorial2.py
@@ -3,7 +3,5 @@ import cv2
 # Reads out image pixels and store as a numpy array
 img = cv2.imread('assets/logo.png', cv2.IMREAD_COLOR)
 
-# Shows the image is a numpy array
-print(img)
-# Outputs (height in px, width in px, channels) of the image
-print(img.shape)
+# Printing pixels value with range
+print(img[257][45:400])
