8b5ea34291bcd5d5b89331ffe34a4fc75a0dcaa9
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 20:00:03 +1200

    Copied image segment and copied to different position

--- tutorial2.py
+++ tutorial2.py
@@ -4,10 +4,10 @@ import random
 # Reads out image pixels and store as a numpy array
 img = cv2.imread('assets/logo.png', cv2.IMREAD_COLOR)
 
-for i in range(100):
-    for j in range(img.shape[1]):
-
-        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+# Getting a pixel slice of an image
+tag = img[200:300, 200: 300]
+# Pick a distination to copy the image segment. Note the source and the distination has to be the same dimension.
+img[100:200, 100:200] = tag
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
