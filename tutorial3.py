ab17074ce31eb845aa359cd667e3de8c72d13be5
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 20:17:19 +1200

    Pasted a video to four segments on a window.

--- tutorial3.py
+++ tutorial3.py
@@ -8,8 +8,19 @@ while True:
     # Returns ret (if the capture worked properly) and frame (image itself in numpy array)
     ret, frame = cap.read()
 
+    # Get a width property and height property
+    # Slice cannot accept float, so need to conver it to int
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
     # Create a blank canvas to show four images
     image = np.zeros(frame.shape, np.uint8)
+    # Shrink the image to one fourth size
+    smaller_frame = cv2.resize(frame, (0, 0), fx = 0.5, fy=0.5)
+    image[:height//2, :width//2] = smaller_frame
+    image[height//2:, :width//2] = smaller_frame
+    image[:height//2, width//2:] = smaller_frame
+    image[height//2:, width//2:] = smaller_frame
     cv2.imshow('Frame', image)
 
     # Wait up to one millisecond and if q is pressed, end showing the video
