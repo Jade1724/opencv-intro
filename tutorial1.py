674bed12a5a33a3198c7969cedcf6d2d8e58ce3f
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 19:41:52 +1200

    Rotate image.

--- tutorial1.py
+++ tutorial1.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread('assets/logo.png', 1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 cv2.imshow('Image', img)
 
