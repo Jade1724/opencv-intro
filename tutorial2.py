1ecb57c81d0dd5f1956eb4c8a1a3f97f50dc38a5
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 19:54:46 +1200

    Change pixels color of a section to random color

--- tutorial2.py
+++ tutorial2.py
@@ -1,7 +1,14 @@
 import cv2
+import random
 
 # Reads out image pixels and store as a numpy array
 img = cv2.imread('assets/logo.png', cv2.IMREAD_COLOR)
 
-# Printing pixels value with range
-print(img[257][45:400])
+for i in range(100):
+    for j in range(img.shape[1]):
+
+        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
